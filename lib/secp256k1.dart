beff1811cb0cdf74182bdb0784547866a8753b27
antonino bajeli <antonino.bajeli@gmail.com>
2019-04-23 08:49:36 +0200

    add settings

--- lib/secp256k1.dart
+++ lib/secp256k1.dart
@@ -10,12 +10,19 @@ import "package:pointycastle/key_generators/ec_key_generator.dart";
 import "package:pointycastle/random/fortuna_random.dart";
 
 void main() {
-  var keyPair = _secp256k1KeyPair();
+  var keyPair = secp256k1KeyPair();
   ECPrivateKey privateKey = keyPair.privateKey;
   print(privateKey.d);
 }
 
-AsymmetricKeyPair<PublicKey, PrivateKey> _secp256k1KeyPair() {
+//97ddae0f3a25b92268175400149d65d6887b9cefaf28ea2c078e05cdc15a3c0a
+//7b83ad6afb1209f3c82ebeb08c0c5fa9bf6724548506f2fb4f991e2287a77090177316ca82b0bdf70cd9dee145c3002c0da1d92626449875972a27807b73b42e
+
+
+//68087b52c11f05ada1d234c9ae4047f6aee57db38511686872af3761dbfb9010
+//af2f601496252506bbfe7000e5a94880b33dbce92585b8ae5c07458a395c8eedaf2f601496252506bbfe7000e5a94880b33dbce92585b8ae5c07458a395c8eed
+
+AsymmetricKeyPair<PublicKey, PrivateKey> secp256k1KeyPair() {
   var keyParams = ECKeyGeneratorParameters(ECCurve_secp256k1());
 
   var random = FortunaRandom();
@@ -33,7 +40,7 @@ Uint8List _seed() {
   return Uint8List.fromList(seed);
 }
 
-}
+
 
 // TODO
 // Restore the ECPrivateKey from 'd'.
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,7 +1,10 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 import 'package:xo/observable_listeners/app_states.dart';
-
+import 'secp256k1.dart';
+import "package:pointycastle/pointycastle.dart";
+import "package:pointycastle/export.dart";
+import "package:pointycastle/ecc/api.dart";
 
 const String sharedPreferenceServerHost='server_host';
 const String defaultServerHostVal="hppt://localhost";
@@ -20,25 +23,71 @@ enum UserSettingDetail {
 
 class Settings {
   static String serverHost="";
+  static String privateKey;
+  static String publicKey;
+  static AsymmetricKeyPair<PublicKey, PrivateKey> keyPair;
+
 
   static final Settings _instance = new Settings.internal();
   factory Settings() => _instance;
 
   Settings.internal();
 
-  static void loadPrefs() {
+   static void loadPrefs() {
     var stateProvider = AppStateProvider();
     Future<SharedPreferences> prefsLoader = SharedPreferences.getInstance();
-    prefsLoader.then((SharedPreferences sharPref) {
-      serverHost = (sharPref.getString(sharedPreferenceServerHost)?? defaultServerHostVal);
+
+    prefsLoader.then((SharedPreferences sharePref) {
+      serverHost = (sharePref.getString(sharedPreferenceServerHost)?? defaultServerHostVal);
       stateProvider.notify(ObserverAppState.INITED);
+
+      privateKey = (sharePref.getString(sharedPreferencePrivateKey)?? getPrivateKey(sharePref,sharedPreferencePrivateKey));
+      publicKey = (sharePref.getString(sharedPreferencePublicKey)?? getPublicKey(sharePref,sharedPreferencePublicKey));
+
+      print("privateKey $privateKey");
+
+      print("publicKey $publicKey");
+
     });
   }
 
+  static String getPrivateKey(SharedPreferences prefs,String _sharedPreferenceKey){
+
+    ECPrivateKey _privateKey;
+    if (keyPair == null) {
+      keyPair = secp256k1KeyPair();
+    }
+    _privateKey = keyPair.privateKey;
+
+    String hexPrivateKey=_privateKey.d.toRadixString(16);
+    // in decimal
+    print(_privateKey.d);
+    // in hex
+    print(hexPrivateKey);
+    prefs.setString(_sharedPreferenceKey, hexPrivateKey);
+    return hexPrivateKey;
+  }
+
+
+  static String getPublicKey(SharedPreferences prefs,String _sharedPreferenceKey){
+    ECPublicKey _publicKey;
+    if (keyPair == null) {
+      keyPair = secp256k1KeyPair();
+    }
+    _publicKey = keyPair.publicKey;
+
+    print(_publicKey.Q.x);
+    print(_publicKey.Q.y);
+    String hexPublicKey=_publicKey.Q.x.toBigInteger().toRadixString(16)+_publicKey.Q.x.toBigInteger().toRadixString(16);
+
+    prefs.setString(_sharedPreferenceKey, hexPublicKey);
+    return hexPublicKey;
+  }
 
   Future<String> getServer() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String serverHost = (prefs.getString('serverHost') ?? defaultServerHost);
+    SharedPreferences sharePref = await SharedPreferences.getInstance();
+    String serverHost = (sharePref.getString(sharedPreferenceServerHost)?? defaultServerHostVal);
     return serverHost;
   }
+
 }
